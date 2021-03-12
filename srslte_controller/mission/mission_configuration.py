1c5d36e170ed7e2defd45ef1a2462ad26945c738
Matan Perelman <matan1008@gmail.com>
2021-03-12 14:49:51 +0200

    MissionConfiguration: Add more defaults to the mission configuration.

--- srslte_controller/mission/mission_configuration.py
+++ srslte_controller/mission/mission_configuration.py
@@ -7,3 +7,7 @@ class MissionConfiguration:
     id: str = field(default_factory=lambda: str(uuid4()))
     mcc: str = '001'
     mnc: str = '01'
+    mme_code: str = '0x1a'
+    mme_group: str = '0x0001'
+    tac: str = '0x0007'
+    apn: str = 'internet'
