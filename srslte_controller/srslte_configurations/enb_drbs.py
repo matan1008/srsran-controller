8dee0a6e64695d42e8e85b2897d7ac1228c6699c
Matan Perelman <matan1008@gmail.com>
2021-03-15 21:08:01 +0200

    srsLTE Configuration: Add drb configuration class.

---
+++ srslte_controller/srslte_configurations/enb_drbs.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass, asdict
+from typing import TextIO
+
+import libconf
+
+
+def to_dict(dataclass_object):
+    return {k: v for k, v in asdict(dataclass_object).items() if v is not None}
+
+
+@dataclass
+class SrsEnbDrbQciConfigPdcpConfig:
+    discard_timer: int = None
+    pdcp_sn_size: int = None
+    status_report_required: bool = None
+
+
+@dataclass
+class SrsEnbDrbQciConfigRlcConfigUlUm:
+    sn_field_length: int
+
+
+@dataclass
+class SrsEnbDrbQciConfigRlcConfigDlUm:
+    sn_field_length: int
+    t_reordering: int
+
+
+@dataclass
+class SrsEnbDrbQciConfigRlcConfigUlAm:
+    t_poll_retx: int
+    poll_pdu: int
+    poll_byte: int
+    max_retx_thresh: int
+
+
+@dataclass
+class SrsEnbDrbQciConfigRlcConfigDlAm:
+    t_reordering: int
+    t_status_prohibit: int
+
+
+@dataclass
+class SrsEnbDrbQciConfigRlcConfig:
+    ul_um: SrsEnbDrbQciConfigRlcConfigUlUm = None
+    dl_um: SrsEnbDrbQciConfigRlcConfigDlUm = None
+    ul_am: SrsEnbDrbQciConfigRlcConfigUlAm = None
+    dl_am: SrsEnbDrbQciConfigRlcConfigDlAm = None
+
+
+@dataclass
+class SrsEnbDrbQciConfigLogicalChannelConfig:
+    priority: int
+    prioritized_bit_rate: int
+    bucket_size_duration: int
+    log_chan_group: int
+
+
+@dataclass
+class SrsEnbDrbQciConfig:
+    qci: int
+    pdcp_config: SrsEnbDrbQciConfigPdcpConfig
+    rlc_config: SrsEnbDrbQciConfigRlcConfig
+    logical_channel_config: SrsEnbDrbQciConfigLogicalChannelConfig
+
+    def to_dict(self):
+        original_dict = asdict(self)
+        original_dict['pdcp_config'] = to_dict(self.pdcp_config)
+        original_dict['rlc_config'] = to_dict(self.rlc_config)
+        return original_dict
+
+
+@dataclass
+class SrsEnbDrbs:
+    qci_config: tuple
+
+    def write(self, fd: TextIO):
+        libconf.dump({'qci_config': tuple(config.to_dict() for config in self.qci_config)}, fd)
---
+++ tests/test_srslte_configurations/test_enb_drbs.py
@@ -0,0 +1,92 @@
+from srslte_controller.srslte_configurations.enb_drbs import *
+from io import StringIO
+
+
+def test_sanity():
+    output = StringIO()
+    SrsEnbDrbs(qci_config=(
+        SrsEnbDrbQciConfig(
+            qci=7,
+            pdcp_config=SrsEnbDrbQciConfigPdcpConfig(discard_timer=100, pdcp_sn_size=12),
+            rlc_config=SrsEnbDrbQciConfigRlcConfig(
+                ul_um=SrsEnbDrbQciConfigRlcConfigUlUm(sn_field_length=10),
+                dl_um=SrsEnbDrbQciConfigRlcConfigDlUm(sn_field_length=10, t_reordering=45)
+            ),
+            logical_channel_config=SrsEnbDrbQciConfigLogicalChannelConfig(
+                priority=13, prioritized_bit_rate=-1, bucket_size_duration=100, log_chan_group=2
+            )
+        ),
+        SrsEnbDrbQciConfig(
+            qci=9,
+            pdcp_config=SrsEnbDrbQciConfigPdcpConfig(discard_timer=-1, status_report_required=True),
+            rlc_config=SrsEnbDrbQciConfigRlcConfig(
+                ul_am=SrsEnbDrbQciConfigRlcConfigUlAm(t_poll_retx=120, poll_pdu=64, poll_byte=750, max_retx_thresh=16),
+                dl_am=SrsEnbDrbQciConfigRlcConfigDlAm(t_reordering=50, t_status_prohibit=50)
+            ),
+            logical_channel_config=SrsEnbDrbQciConfigLogicalChannelConfig(
+                priority=11, prioritized_bit_rate=-1, bucket_size_duration=100, log_chan_group=3
+            )
+        ),
+    )).write(output)
+    assert output.getvalue() == (
+        'qci_config =\n'
+        '(\n'
+        '    {\n'
+        '        qci = 7;\n'
+        '        pdcp_config =\n'
+        '        {\n'
+        '            discard_timer = 100;\n'
+        '            pdcp_sn_size = 12;\n'
+        '        };\n'
+        '        rlc_config =\n'
+        '        {\n'
+        '            ul_um =\n'
+        '            {\n'
+        '                sn_field_length = 10;\n'
+        '            };\n'
+        '            dl_um =\n'
+        '            {\n'
+        '                sn_field_length = 10;\n'
+        '                t_reordering = 45;\n'
+        '            };\n'
+        '        };\n'
+        '        logical_channel_config =\n'
+        '        {\n'
+        '            priority = 13;\n'
+        '            prioritized_bit_rate = -1;\n'
+        '            bucket_size_duration = 100;\n'
+        '            log_chan_group = 2;\n'
+        '        };\n'
+        '    },\n'
+        '    {\n'
+        '        qci = 9;\n'
+        '        pdcp_config =\n'
+        '        {\n'
+        '            discard_timer = -1;\n'
+        '            status_report_required = True;\n'
+        '        };\n'
+        '        rlc_config =\n'
+        '        {\n'
+        '            ul_am =\n'
+        '            {\n'
+        '                t_poll_retx = 120;\n'
+        '                poll_pdu = 64;\n'
+        '                poll_byte = 750;\n'
+        '                max_retx_thresh = 16;\n'
+        '            };\n'
+        '            dl_am =\n'
+        '            {\n'
+        '                t_reordering = 50;\n'
+        '                t_status_prohibit = 50;\n'
+        '            };\n'
+        '        };\n'
+        '        logical_channel_config =\n'
+        '        {\n'
+        '            priority = 11;\n'
+        '            prioritized_bit_rate = -1;\n'
+        '            bucket_size_duration = 100;\n'
+        '            log_chan_group = 3;\n'
+        '        };\n'
+        '    }\n'
+        ');\n'
+    )
