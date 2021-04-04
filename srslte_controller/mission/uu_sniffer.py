19ad7d101ac86b86262527c23d39dc1280970e23
Matan Perelman <matan1008@gmail.com>
2021-04-04 23:45:38 +0300

    UuSniffer: Since the cap file end might be corrupted, read packets in chunks.

--- srslte_controller/mission/uu_sniffer.py
+++ srslte_controller/mission/uu_sniffer.py
@@ -6,15 +6,17 @@ from pyshark.capture.capture import TSharkCrashException
 
 
 class AsyncFileCapture(pyshark.FileCapture):
-    async def async_load_packets(self):
+    async def async_load_packets(self, packet_count=None):
         """
         Reads the packets from the source (cap, interface, etc.) and adds it to the internal list.
         """
-        await self.packets_from_tshark(self._packets.append)
+        await self.packets_from_tshark(self._packets.append, packet_count=packet_count)
         self.loaded = True
 
 
 class UuSniffer:
+    READING_CHUNK = 20
+
     def __init__(self, uu_cap, parser):
         self._cap_index = 0
         self._run = True
@@ -29,11 +31,15 @@ class UuSniffer:
             await asyncio.sleep(0)
 
         # We use our own version of FileCapture since the original is not really async.
-        cap = AsyncFileCapture(self._uu_cap, display_filter='!lte-rrc.BCCH_DL_SCH_Message_element')
+        cap = AsyncFileCapture(self._uu_cap)
         while self._run:
-            await self._reload_cap(cap)
-            self._publish_new_packets(cap)
-            self._cap_index = len(cap)
+            # TShark might crash because the last packet might not be fully written, it will be read on the next reload.
+            try:
+                await self._reload_cap(cap)
+                self._publish_new_packets(cap)
+                self._cap_index = len(cap)
+            except TSharkCrashException:
+                continue
 
     def stop(self):
         """
@@ -42,18 +48,13 @@ class UuSniffer:
         self._run = False
 
     async def _reload_cap(self, cap):
-        # TShark might crash because the last packet might not be fully written, it will be read on the next reload.
-        while self._run:
-            try:
-                cap.clear()
-                await cap.async_load_packets()
-            except TSharkCrashException:
-                continue
-            else:
-                break
+        cap.clear()
+        # Read packets in chunks since PCAP file end might be corrupted.
+        await cap.async_load_packets(packet_count=self._cap_index + self.READING_CHUNK)
 
     def _publish_new_packets(self, cap):
         for i, packet in enumerate(cap):
             if i < self._cap_index:
+                # Packets already read.
                 continue
             self._parser.from_packet(packet)
