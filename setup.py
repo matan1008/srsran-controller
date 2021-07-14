802806c696db5903dd9d197d8cbd266161dfe0f2
Matan Perelman <matan1008@gmail.com>
2021-07-14 02:43:42 -0400

    setup.py: Make package installable.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    name='srsran-controller',
+    packages=find_packages(),
     install_requires=['docker', 'libconf', 'pyshark'],
     tests_require=['pytest'],
 )
