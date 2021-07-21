8bad60c30381b241a86a52d2c258fa7567be64e8
Matan Perelman <matan1008@gmail.com>
2021-07-21 10:45:25 -0400

    Uu events: Try to convert tmsi to imsi if possible.

--- srsran_controller/common/ip.py
+++ srsran_controller/common/ip.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import netifaces
 
 from srsran_controller.common.utils import run_as_sudo
@@ -29,6 +31,33 @@ def construct_forward(from_, to):
 def forward_interfaces(from_, to, password):
     for iptable_command in construct_forward(from_, to):
         run_as_sudo(iptable_command, password)
+
+
+class UdpSendReceiveProtocol(asyncio.DatagramProtocol):
+    def __init__(self, message, on_receive):
+        self.message = message
+        self.on_receive = on_receive
+        self.transport = None
+
+    def connection_made(self, transport: asyncio.DatagramTransport):
+        self.transport = transport
+        self.transport.sendto(self.message)
+
+    def datagram_received(self, data, addr):
+        self.on_receive.set_result(data)
+        self.transport.close()
+
+
+async def asyncio_udp_send_receive(data, ip, port):
+    loop = asyncio.get_running_loop()
+    on_receive = loop.create_future()
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: UdpSendReceiveProtocol(data, on_receive),
+        remote_addr=(ip, port))
+    try:
+        return await on_receive
+    finally:
+        transport.close()
 
 
 def find_interface_of_address(ip_address):
