c76143a672719a713f00d7b681d803e5d2c35e25
Matan Perelman <matan1008@gmail.com>
2021-08-08 07:55:48 +0300

    SubscribersManager: Fix line termination and SQN formatting.

--- srsran_controller/subscribers_manager.py
+++ srsran_controller/subscribers_manager.py
@@ -1,5 +1,5 @@
 import csv
-from dataclasses import dataclass, fields, asdict
+from dataclasses import dataclass
 
 from srsran_controller.configuration import config
 
@@ -24,41 +24,42 @@ class Subscriber:
 class SubscribersManager:
     def create_subscriber(self, name, imsi, key, op_type, op, amf, sqn, qci, ip, auth='mil'):
         with open(config.users_db, 'a') as subscribers_file:
-            writer = csv.writer(subscribers_file)
-            writer.writerow([name, auth, imsi, key, op_type, op, amf, sqn, qci, ip])
+            writer = csv.writer(subscribers_file, lineterminator='\n')
+            writer.writerow([name, auth, imsi, key, op_type, op, amf, f'{sqn:012x}', qci, ip])
         return list(self.iter_subscribers())[-1]
 
-    @staticmethod
-    def edit_subscriber(subscriber: Subscriber):
-        with open(config.users_db) as subscribers_file:
-            lines = list(csv.reader(subscribers_file))
+    def edit_subscriber(self, subscriber: Subscriber):
+        lines = list(self._iter_db_lines())
         with open(config.users_db, 'w') as subscribers_file:
-            writer = csv.writer(subscribers_file)
+            writer = csv.writer(subscribers_file, lineterminator='\n')
             for i, row in enumerate(lines):
                 if i == subscriber.index:
-                    writer.writerow([asdict(subscriber)[field] for field in CSV_COLUMNS])
+                    writer.writerow([
+                        subscriber.name, subscriber.auth, subscriber.imsi, subscriber.key, subscriber.op_type,
+                        subscriber.op, subscriber.amf, f'{subscriber.sqn:012x}', subscriber.qci, subscriber.ip
+                    ])
                 else:
                     writer.writerow(row)
 
-    @staticmethod
-    def delete_subscriber(subscriber: Subscriber):
-        with open(config.users_db) as subscribers_file:
-            lines = list(csv.reader(subscribers_file))
+    def delete_subscriber(self, subscriber: Subscriber):
+        lines = list(self._iter_db_lines())
         with open(config.users_db, 'w') as subscribers_file:
-            writer = csv.writer(subscribers_file)
+            writer = csv.writer(subscribers_file, lineterminator='\n')
             for i, row in enumerate(lines):
                 if i == subscriber.index:
                     continue
                 else:
                     writer.writerow(row)
 
+    def iter_subscribers(self):
+        for i, row in enumerate(self._iter_db_lines()):
+            # The row is in the following format: Name,Auth,IMSI,Key,OP_Type,OP/OPc,AMF,SQN,QCI,IP_alloc
+            yield Subscriber(i, row[0], row[2], row[3], row[4], row[5], row[6], int(row[7], 16), int(row[8]), row[9],
+                             row[1])
+
     @staticmethod
-    def iter_subscribers():
+    def _iter_db_lines():
         with open(config.users_db) as subscribers_file:
-            reader = csv.reader(subscribers_file)
-            for i, row in enumerate(reader):
-                data = {}
-                for column_index, column in enumerate(CSV_COLUMNS):
-                    field = next(f for f in fields(Subscriber) if f.name == column)
-                    data[column] = field.type(row[column_index])
-                yield Subscriber(i, **data)
+            reader = csv.reader(filter(lambda line: line[0] != '#', subscribers_file))
+            for row in reader:
+                yield row
--- tests/test_subscribers_manager.py
+++ tests/test_subscribers_manager.py
@@ -1,7 +1,7 @@
 import pytest
 
 from srsran_controller.configuration import config
-from srsran_controller.subscribers_manager import SubscribersManager
+from srsran_controller.subscribers_manager import SubscribersManager, Subscriber
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -30,7 +30,7 @@ def test_adding_subscribers(users_db_file):
     assert sub.ip == 'dynamic'
     assert sub.auth == 'mil'
     assert users_db_file.read_text() == ('Name,mil,001010123456785,00112233445566778899aabbccddeeff,opc,'
-                                         '63bfa50ee6523365ff14c1f45f88737d,9001,1233,9,dynamic\n')
+                                         '63bfa50ee6523365ff14c1f45f88737d,9001,0000000004d1,9,dynamic\n')
 
 
 def test_deleting_subscribers(users_db_file):
@@ -41,7 +41,7 @@ def test_deleting_subscribers(users_db_file):
                                '63bfa50ee6523365ff14c1f45f88737d', '9001', 1233, 9, 'dynamic')
     s.delete_subscriber(sub)
     assert users_db_file.read_text() == ('Name2,mil,001010123456783,00112233445566778899aabbccddeeff,opc,'
-                                         '63bfa50ee6523365ff14c1f45f88737d,9001,1233,9,dynamic\n')
+                                         '63bfa50ee6523365ff14c1f45f88737d,9001,0000000004d1,9,dynamic\n')
     sub2.index -= 1
     s.delete_subscriber(sub2)
     assert not users_db_file.read_text()
@@ -56,6 +56,20 @@ def test_iterating_subscribers():
     assert [sub, sub2] == list(s.iter_subscribers())
 
 
+def test_iterating_subscribers_with_comment(users_db_file):
+    users_db_file.write_text(
+        '# Name,mil,001010123456785,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,1233,9,'
+        'dynamic\n'
+        'Name2,mil,001010123456783,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,1233,9,'
+        'dynamic\n'
+    )
+    s = SubscribersManager()
+    assert list(s.iter_subscribers()) == [Subscriber(
+        0, 'Name2', '001010123456783', '00112233445566778899aabbccddeeff', 'opc', '63bfa50ee6523365ff14c1f45f88737d',
+        '9001', 4659, 9, 'dynamic'
+    )]
+
+
 def test_editing_subscribers(users_db_file):
     s = SubscribersManager()
     sub = s.create_subscriber('Name', '001010123456785', '00112233445566778899aabbccddeeff', 'opc',
@@ -65,8 +79,8 @@ def test_editing_subscribers(users_db_file):
     sub.name = 'New Name!'
     s.edit_subscriber(sub)
     assert users_db_file.read_text() == (
-        'New Name!,mil,001010123456785,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,1233,'
-        '9,dynamic\n'
-        'Name2,mil,001010123456783,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,1233,'
-        '9,dynamic\n'
+        'New Name!,mil,001010123456785,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,'
+        '0000000004d1,9,dynamic\n'
+        'Name2,mil,001010123456783,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,'
+        '0000000004d1,9,dynamic\n'
     )
