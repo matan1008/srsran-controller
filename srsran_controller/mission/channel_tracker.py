25634c56c40fae0a3f742c2d9b443e3589a6f7de
Matan Perelman <matan1008@gmail.com>
2021-08-16 11:54:23 +0300

    ChannelTracker: Add documentation.

--- srsran_controller/mission/channel_tracker.py
+++ srsran_controller/mission/channel_tracker.py
@@ -25,7 +25,11 @@ class ChannelTracker:
             IDENTITY_RESPONSE_NAME: self._handle_identity_response,
         }
 
-    def enrich_event(self, event: dict):
+    def enrich_event(self, event: dict) -> None:
+        """
+        Add additional data to a Uu event, based on the RNTI.
+        :param event: Uu event to add data to.
+        """
         if 'rnti' not in event:
             return
         channel_metadata = self._rnti_channels[event['rnti']]
@@ -37,24 +41,28 @@ class ChannelTracker:
             event['imeisv'] = channel_metadata.imeisv
 
     def handle_uu_event(self, event: dict) -> None:
+        """
+        Cache possible data from an event on the Uu interface
+        :param event: Uu event to extract data from.
+        """
         if event['event'] not in self._events_handlers:
             return
         self._events_handlers[event['event']](event)
 
-    def _handle_rar(self, event):
+    def _handle_rar(self, event: dict):
         self._rnti_channels[event['c-rnti']] = ChannelMetadata(ta=event['ta'])
 
-    def _handle_attach_request(self, event):
+    def _handle_attach_request(self, event: dict):
         self._rnti_channels[event['rnti']].imsi = event['imsi']
 
-    def _handle_security_mode_complete(self, event):
+    def _handle_security_mode_complete(self, event: dict):
         if 'imeisv' in event:
             self._rnti_channels[event['rnti']].imeisv = event['imeisv']
 
-    def _handle_connection_request(self, event):
+    def _handle_connection_request(self, event: dict):
         if 'imsi' in event:
             self._rnti_channels[event['rnti']].imsi = event['imsi']
 
-    def _handle_identity_response(self, event):
+    def _handle_identity_response(self, event: dict):
         if 'imsi' in event:
             self._rnti_channels[event['rnti']].imsi = event['imsi']
