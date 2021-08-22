78dfcf09d6827b7e487266c879491d7e54411bfe
Matan Perelman <matan1008@gmail.com>
2021-08-23 00:20:37 +0300

    ChannelTracker: Add ip following.

--- srsran_controller/mission/channel_tracker.py
+++ srsran_controller/mission/channel_tracker.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from srsran_controller.uu_events.nas_emm_attach_accept import ATTACH_ACCEPT_NAME
 from srsran_controller.uu_events.nas_emm_attach_request import ATTACH_REQUEST_NAME
 from srsran_controller.uu_events.nas_emm_identity_response import IDENTITY_RESPONSE_NAME
 from srsran_controller.uu_events.nas_emm_security_mode_complete import SECURITY_MODE_COMPLETE_NAME
@@ -12,18 +13,30 @@ class ChannelMetadata:
     ta: int
     imsi: str = ''
     imeisv: str = ''
+    ip: str = ''
 
 
 class ChannelTracker:
     def __init__(self):
         self._rnti_channels = {}
         self._events_handlers = {
+            ATTACH_ACCEPT_NAME: self._handle_attach_accept,
             ATTACH_REQUEST_NAME: self._handle_attach_request,
             SECURITY_MODE_COMPLETE_NAME: self._handle_security_mode_complete,
             RA_RESPONSE_NAME: self._handle_rar,
             CONNECTION_REQUEST_NAME: self._handle_connection_request,
             IDENTITY_RESPONSE_NAME: self._handle_identity_response,
         }
+
+    def imsi_to_ip(self, imsi: str) -> str:
+        """
+        Fetch the last ip seen given to a subscriber.
+        :param imsi: Subscriber's IMSI.
+        :return: Subscriber's IP.
+        """
+        for channel in self._rnti_channels.values():
+            if channel.imsi == imsi:
+                return channel.ip
 
     def enrich_event(self, event: dict) -> None:
         """
@@ -39,6 +52,8 @@ class ChannelTracker:
             event['imsi'] = channel_metadata.imsi
         if 'imeisv' not in event and channel_metadata.imeisv:
             event['imeisv'] = channel_metadata.imeisv
+        if 'ip' not in event and channel_metadata.ip:
+            event['ip'] = channel_metadata.ip
 
     def handle_uu_event(self, event: dict) -> None:
         """
@@ -51,6 +66,9 @@ class ChannelTracker:
 
     def _handle_rar(self, event: dict):
         self._rnti_channels[event['c-rnti']] = ChannelMetadata(ta=event['ta'])
+
+    def _handle_attach_accept(self, event: dict):
+        self._rnti_channels[event['rnti']].ip = event['ip']
 
     def _handle_attach_request(self, event: dict):
         self._rnti_channels[event['rnti']].imsi = event['imsi']
