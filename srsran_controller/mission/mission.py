b57d483c2674bc95ecd9e0946eb3a032f571b9b6
Matan Perelman <matan1008@gmail.com>
2021-10-26 15:57:34 +0300

    Mission: Add Uu events logging.

--- srsran_controller/mission/mission.py
+++ srsran_controller/mission/mission.py
@@ -1,21 +1,23 @@
 import asyncio
 from datetime import datetime
+from logging import Logger, getLogger
 
 from srsran_controller.mission.channel_tracker import ChannelTracker
 from srsran_controller.mission.ping import Ping
-from srsran_controller.uu_events.uu_sniffer import UuSniffer
 from srsran_controller.uu_events.factory import EventsFactory
+from srsran_controller.uu_events.uu_sniffer import UuSniffer
 
 
 class Mission:
 
-    def __init__(self, epc, enb, lte_network, pgw_network=None):
+    def __init__(self, epc, enb, lte_network, pgw_network=None, logger: Logger = getLogger('srsran_controller')):
         """
         Create a new mission object.
         :param srsran_controller.mission.epc.Epc epc: Mission's EPC.
         :param srsran_controller.mission.enb.Enb enb: Mission's ENB.
         :param srsran_controller.mission.lte_network.LteNetwork lte_network: Mission's network.
         :param srsran_controller.mission.pgw_network.PgwNetwork pgw_network: P-GW network.
+        :param logger: Logger.
         """
         self.uu_events = []
         self.uu_events_callback = lambda event: None
@@ -26,6 +28,7 @@ class Mission:
         self.enb = enb
         self._lte_network = lte_network
         self._pgw_network = pgw_network
+        self.logger = logger
         self.pings = []
         self.start_time = datetime.now()
         self._sniffing_task = asyncio.create_task(self._sniff_packets())
@@ -71,6 +74,7 @@ class Mission:
                 self._handle_uu_event(event)
 
     def _handle_uu_event(self, event):
+        self.logger.debug(f'Handle Uu event: {event}')
         self.uu_events.append(event)
         self.channel_tracker.handle_uu_event(event)
         self.channel_tracker.enrich_event(event)
