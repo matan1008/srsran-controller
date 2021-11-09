f7c66648a44e1faddf2afa1d547cca78fc7cc788
Matan Perelman <matan1008@gmail.com>
2021-11-09 14:32:49 +0200

    Uu events: Add TMSI parsing to attach accept.

--- srsran_controller/uu_events/nas_emm_attach_accept.py
+++ srsran_controller/uu_events/nas_emm_attach_accept.py
@@ -8,6 +8,7 @@ def create(pkt):
         if int(mac_layer.nas_eps_nas_msg_emm_type) == NAS_EMM_TYPE_ATTACH_ACCEPT:
             return {
                 'ip': getattr(mac_layer, 'nas_eps.esm.pdn_ipv4'),
+                'tmsi': getattr(mac_layer, 'nas_eps.emm.m_tmsi'),
                 'event': ATTACH_ACCEPT_NAME,
                 'rnti': int(mac_layer.rnti)
             }
