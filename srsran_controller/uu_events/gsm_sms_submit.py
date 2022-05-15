0d07971e6cca023619da450495461233738030ea
Matan Perelman <matan1008@gmail.com>
2022-05-15 08:45:32 +0300

    Uu events: Support utf-16 in SMS submit.

--- srsran_controller/uu_events/gsm_sms_submit.py
+++ srsran_controller/uu_events/gsm_sms_submit.py
@@ -5,6 +5,10 @@ GSM_SMS_SUBMIT_NAME = 'SMS Submit'
 def create(pkt):
     try:
         mac_layer = pkt['mac-lte']
+        if hasattr(mac_layer, 'gsm_sms_dcs_character_set') and int(mac_layer.gsm_sms_dcs_character_set, 0) == 2:
+            text = mac_layer.gsm_sms_sms_text.main_field.binary_value.decode('utf-16be')
+        else:
+            text = str(mac_layer.gsm_sms_sms_text)
         if int(mac_layer.gsm_sms_tp_mti) == TP_MTI_SMS_SUBMIT:
             return {
                 'event': GSM_SMS_SUBMIT_NAME,
@@ -12,7 +16,7 @@ def create(pkt):
                 'data': {
                     'rp_da': str(mac_layer.gsm_a_dtap_cld_party_bcd_num),
                     'tp_da': str(mac_layer.gsm_sms_tp_da),
-                    'content': str(mac_layer.gsm_sms_sms_text),
+                    'content': text,
                 }
             }
     except (KeyError, AttributeError):
