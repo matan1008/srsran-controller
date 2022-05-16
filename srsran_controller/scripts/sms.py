9bd831fbeb8682b7433b469bd681c53614e1ae2f
Matan Perelman <matan1008@gmail.com>
2022-05-16 17:36:00 +0300

    Sms: Add SMS script.

---
+++ srsran_controller/scripts/sms.py
@@ -0,0 +1,59 @@
+import time
+
+from pycrate_mobile.TS23040_SMS import *
+from pycrate_mobile.TS24011_PPSMS import *
+
+from srsran_controller.scripts.abstract import AbstractScript
+from srsran_controller.uu_events.gsm_cp_ack import CP_ACK_MTI
+from srsran_controller.uu_events.gsm_rp_ack import RP_ACK_MESSAGE_TYPE
+
+
+def is_cp_ack(pkt):
+    mac_lte = pkt['mac-lte']
+    return int(mac_lte.direction) == 0 and int(mac_lte.gsm_a_dtap_msg_sms_type, 0) == CP_ACK_MTI
+
+
+def is_rp_ack(pkt):
+    try:
+        mac_lte = pkt['mac-lte']
+        return int(mac_lte.direction) == 0 and int(mac_lte.gsm_a_rp_msg_type, 0) == RP_ACK_MESSAGE_TYPE
+    except (KeyError, AttributeError):
+        return False
+
+
+class Sms(AbstractScript):
+    def __init__(self, text: str, rp_oa: str, tp_oa: str):
+        super().__init__()
+        self.text = text
+        self.rp_oa = rp_oa
+        self.tp_oa = tp_oa
+
+    def _construct_sms(self, rp_oa, tp_oa, text):
+        deliver = SMS_DELIVER(val={
+            'TP_MMS': 1,
+            'TP_OA': {'Num': tp_oa, 'Type': 5, 'NumberingPlan': 8},
+            'TP_DCS': {'Group': 1, 'Class': 1, 'Charset': 2},
+            'TP_SCTS': (time.localtime(), time.altzone),
+            'TP_UD': {'UD': text}
+        })
+        rp_data = RP_DATA_MT()
+        rp_data.set_tpdu(deliver)
+        rp_data['RPOriginatorAddress'].set_IE(val={'Num': rp_oa})
+        cp_data = CP_DATA()
+        cp_data.set_rp(rp_data)
+        return cp_data.to_bytes()
+
+    async def run(self):
+        await self.ecm_connect()
+        cp_data = self._construct_sms(self.rp_oa, self.tp_oa, self.text)
+        self.log(f'Sending downlink nas transport {cp_data.hex()}')
+        await self.mission.epc.send_downlink_nas_transport(self.imsi, cp_data)
+        packet = await self.wait_for_parsed_packet(is_cp_ack)
+        self.log('CP-ACK Received')
+        # CP-ACK and RP-ACK might be sent in the same packet.
+        if not is_rp_ack(packet):
+            await self.wait_for_parsed_packet(is_rp_ack)
+        self.log('RP-ACK Received')
+        cp_ack = CP_ACK()
+        self.log(f'Sending downlink nas transport {cp_ack.to_bytes().hex()}')
+        await self.mission.epc.send_downlink_nas_transport(self.imsi, cp_ack.to_bytes())
