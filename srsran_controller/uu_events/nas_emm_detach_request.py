ebf8a0d3dd0626b0ac57880d057a64eac8759ac8
Matan Perelman <matan1008@gmail.com>
2022-05-17 15:33:57 +0300

    Uu events: Export detach request name.

--- srsran_controller/uu_events/nas_emm_detach_request.py
+++ srsran_controller/uu_events/nas_emm_detach_request.py
@@ -1,4 +1,5 @@
 NAS_EMM_TYPE_DETACH_REQUEST = 0x45
+DETACH_REQUEST_NAME = 'Detach request'
 
 
 def create(pkt):
@@ -7,7 +8,7 @@ def create(pkt):
         if int(mac_layer.nas_eps_nas_msg_emm_type) == NAS_EMM_TYPE_DETACH_REQUEST:
             return {
                 'tmsi': mac_layer.nas_eps_emm_m_tmsi,
-                'event': 'Detach request',
+                'event': DETACH_REQUEST_NAME,
                 'rnti': int(mac_layer.rnti)
             }
     except (KeyError, AttributeError):
