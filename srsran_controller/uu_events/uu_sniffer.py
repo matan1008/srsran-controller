724864683a84550c08ae4aaca7919874b9b45352
Matan Perelman <matan1008@gmail.com>
2022-06-21 08:26:08 +0300

    UuSniffer: Use pyshark correctly.

--- srsran_controller/uu_events/uu_sniffer.py
+++ srsran_controller/uu_events/uu_sniffer.py
@@ -2,35 +2,7 @@ import asyncio
 import socket
 from contextlib import closing
 
-import pyshark
-
-
-class AsyncLiveCapture(pyshark.LiveCapture):
-    async def sniff_continuously(self, packet_count=None):
-        tshark_process = await self._get_tshark_process()
-        psml_structure, data = await self._get_psml_struct(tshark_process.stdout)
-        packets_captured = 0
-
-        data = b''
-        try:
-            while True:
-                try:
-                    packet, data = await self._get_packet_from_stream(tshark_process.stdout, data,
-                                                                      psml_structure=psml_structure,
-                                                                      got_first_packet=packets_captured > 0)
-                except EOFError:
-                    self._log.debug('EOF reached (sync)')
-                    self._eof_reached = True
-                    break
-
-                if packet:
-                    packets_captured += 1
-                    yield packet
-                if packet_count and packets_captured >= packet_count:
-                    break
-        finally:
-            if tshark_process in self._running_processes:
-                await self._cleanup_subprocess(tshark_process)
+from pyshark import LiveCapture
 
 
 class UuSniffer:
@@ -49,10 +21,12 @@ class UuSniffer:
         # The socket is required in order to prevent ICMP destination port unreachable from being sent.
         with closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as sock:
             sock.bind((self._addr, self._port))
-            try:
-                async with AsyncLiveCapture(bpf_filter=f'udp port {self._port}', interface=self._interface,
-                                            eventloop=asyncio.get_event_loop(), use_json=use_json) as cap:
-                    async for packet in cap.sniff_continuously():
-                        yield packet
-            except asyncio.CancelledError:
-                pass
+            packet_queue = asyncio.Queue()
+            async with LiveCapture(bpf_filter=f'udp port {self._port}', interface=self._interface, use_json=use_json,
+                                   eventloop=asyncio.get_event_loop()) as cap:
+                cap_task = asyncio.create_task(cap.packets_from_tshark(packet_queue.put_nowait))
+                try:
+                    while True:
+                        yield await packet_queue.get()
+                except asyncio.CancelledError:
+                    cap_task.cancel()
